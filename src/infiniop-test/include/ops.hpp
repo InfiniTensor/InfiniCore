2f7ce76704cd745f99f0b14d4465ec17210550cb
YdrMaster <ydrml@hotmail.com>
2025-04-08 08:04:49 +0000

    issue/152/feat: 添加 rearrange 算子测例
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/infiniop-test/include/ops.hpp
+++ src/infiniop-test/include/ops.hpp
@@ -7,6 +7,7 @@
  */
 DECLARE_INFINIOP_TEST(gemm)
 DECLARE_INFINIOP_TEST(random_sample)
+DECLARE_INFINIOP_TEST(rearrange)
 
 #define REGISTER_INFINIOP_TEST(name)                      \
     {                                                     \
@@ -24,12 +25,25 @@ DECLARE_INFINIOP_TEST(random_sample)
     {                                         \
         REGISTER_INFINIOP_TEST(gemm)          \
         REGISTER_INFINIOP_TEST(random_sample) \
+        REGISTER_INFINIOP_TEST(rearrange)     \
     }
 
 namespace infiniop_test {
 
 // Global variable for {op_name: builder} mappings
 extern std::unordered_map<std::string, const TestBuilder> TEST_BUILDERS;
+
+template <typename V>
+bool check_names(
+    const std::unordered_map<std::string, V> &map,
+    const std::vector<std::string> &names) {
+    for (auto const &name : names) {
+        if (map.find(name) == map.end()) {
+            return false;
+        }
+    }
+    return true;
+}
 
 } // namespace infiniop_test
 
