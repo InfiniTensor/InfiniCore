1285badd4eae78996bb3b2dbb090c5d54854477c
Kinorw <otherlittle66@gmail.com>
2025-11-29 19:05:19 +0800

    fix: rearrange分配最小Unit未考虑带有strides的情况能否被整除

--- src/infinicore/ops/bilinear/bilinear.cc
+++ src/infinicore/ops/bilinear/bilinear.cc
@@ -1,5 +1,5 @@
 #include "infinicore/ops/bilinear.hpp"
-#include "infinicore/ops/gemm.hpp"
+#include "infinicore/ops/matmul.hpp"
 #include "infinicore/ops/add.hpp"
 #include "infinicore/ops/rearrange.hpp"
 
@@ -15,7 +15,6 @@ Tensor bilinear(Tensor x1, Tensor x2, Tensor weight, std::optional<Tensor> bias)
 
     auto dtype = x1->dtype();
     auto device = x1->device();
-
     Tensor x1_cont = x1->is_contiguous() ? x1 : x1->contiguous();
     Tensor x2_cont = x2->is_contiguous() ? x2 : x2->contiguous();
     Tensor weight_cont = weight->is_contiguous() ? weight : weight->contiguous();
@@ -24,21 +23,19 @@ Tensor bilinear(Tensor x1, Tensor x2, Tensor weight, std::optional<Tensor> bias)
     Tensor weight_permuted_cont = weight_permuted->contiguous();
     Tensor weight_matrix = weight_permuted_cont->view({in1_features, out_features * in2_features});
 
-    Tensor intermediate = gemm(x1_cont, weight_matrix);
+    Tensor intermediate = matmul(x1_cont, weight_matrix);
 
     Tensor intermediate_3d = intermediate->view({batch_size, out_features, in2_features});
 
     Tensor x2_col = x2_cont->view({batch_size, in2_features, 1});
 
-    Tensor out_3d = gemm(intermediate_3d, x2_col);
-
+    Tensor out_3d = matmul(intermediate_3d, x2_col);
     Tensor out = out_3d->view({batch_size, out_features});
 
     if (bias) {
         Tensor bias_broadcast = (*bias)->as_strided({batch_size, out_features}, {0, (*bias)->strides()[0]});
         out = add(out, bias_broadcast);
     }
-
     return out;
 }
 
