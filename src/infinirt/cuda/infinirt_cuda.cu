8b73dbc1019156c96d7f312ce5d2d67d2dab0d73
zhuyue <zhuyue@qiyuanlab.com>
2025-12-01 11:01:26 +0800

    Issue/685 - Refactor infinirt multi-device support.

--- src/infinirt/cuda/infinirt_cuda.cu
+++ src/infinirt/cuda/infinirt_cuda.cu
@@ -4,7 +4,9 @@
 
 #define CHECK_CUDART(RT_API) CHECK_INTERNAL(RT_API, cudaSuccess)
 
-namespace infinirt::cuda {
+// Shared implementation for all CUDA-compatible devices
+namespace infinirt::cuda_impl {
+
 infiniStatus_t getDeviceCount(int *count) {
     CHECK_CUDART(cudaGetDeviceCount(count));
     return INFINI_STATUS_SUCCESS;
@@ -156,4 +158,376 @@ infiniStatus_t freeAsync(void *ptr, infinirtStream_t stream) {
     CHECK_CUDART(cudaFreeAsync(ptr, (cudaStream_t)stream));
     return INFINI_STATUS_SUCCESS;
 }
+} // namespace infinirt::cuda_impl
+
+#ifdef ENABLE_NVIDIA_API
+// NVIDIA namespace - wraps shared implementation
+namespace infinirt::cuda {
+infiniStatus_t getDeviceCount(int *count) {
+    return infinirt::cuda_impl::getDeviceCount(count);
+}
+
+infiniStatus_t setDevice(int device_id) {
+    return infinirt::cuda_impl::setDevice(device_id);
+}
+
+infiniStatus_t deviceSynchronize() {
+    return infinirt::cuda_impl::deviceSynchronize();
+}
+
+infiniStatus_t streamCreate(infinirtStream_t *stream_ptr) {
+    return infinirt::cuda_impl::streamCreate(stream_ptr);
+}
+
+infiniStatus_t streamDestroy(infinirtStream_t stream) {
+    return infinirt::cuda_impl::streamDestroy(stream);
+}
+
+infiniStatus_t streamSynchronize(infinirtStream_t stream) {
+    return infinirt::cuda_impl::streamSynchronize(stream);
+}
+
+infiniStatus_t streamWaitEvent(infinirtStream_t stream, infinirtEvent_t event) {
+    return infinirt::cuda_impl::streamWaitEvent(stream, event);
+}
+
+infiniStatus_t eventCreate(infinirtEvent_t *event_ptr) {
+    return infinirt::cuda_impl::eventCreate(event_ptr);
+}
+
+infiniStatus_t eventCreateWithFlags(infinirtEvent_t *event_ptr, uint32_t flags) {
+    return infinirt::cuda_impl::eventCreateWithFlags(event_ptr, flags);
+}
+
+infiniStatus_t eventRecord(infinirtEvent_t event, infinirtStream_t stream) {
+    return infinirt::cuda_impl::eventRecord(event, stream);
+}
+
+infiniStatus_t eventQuery(infinirtEvent_t event, infinirtEventStatus_t *status_ptr) {
+    return infinirt::cuda_impl::eventQuery(event, status_ptr);
+}
+
+infiniStatus_t eventSynchronize(infinirtEvent_t event) {
+    return infinirt::cuda_impl::eventSynchronize(event);
+}
+
+infiniStatus_t eventDestroy(infinirtEvent_t event) {
+    return infinirt::cuda_impl::eventDestroy(event);
+}
+
+infiniStatus_t eventElapsedTime(float *ms_ptr, infinirtEvent_t start, infinirtEvent_t end) {
+    return infinirt::cuda_impl::eventElapsedTime(ms_ptr, start, end);
+}
+
+infiniStatus_t mallocDevice(void **p_ptr, size_t size) {
+    return infinirt::cuda_impl::mallocDevice(p_ptr, size);
+}
+
+infiniStatus_t mallocHost(void **p_ptr, size_t size) {
+    return infinirt::cuda_impl::mallocHost(p_ptr, size);
+}
+
+infiniStatus_t freeDevice(void *ptr) {
+    return infinirt::cuda_impl::freeDevice(ptr);
+}
+
+infiniStatus_t freeHost(void *ptr) {
+    return infinirt::cuda_impl::freeHost(ptr);
+}
+
+infiniStatus_t memcpy(void *dst, const void *src, size_t size, infinirtMemcpyKind_t kind) {
+    return infinirt::cuda_impl::memcpy(dst, src, size, kind);
+}
+
+infiniStatus_t memcpyAsync(void *dst, const void *src, size_t size, infinirtMemcpyKind_t kind, infinirtStream_t stream) {
+    return infinirt::cuda_impl::memcpyAsync(dst, src, size, kind, stream);
+}
+
+infiniStatus_t mallocAsync(void **p_ptr, size_t size, infinirtStream_t stream) {
+    return infinirt::cuda_impl::mallocAsync(p_ptr, size, stream);
+}
+
+infiniStatus_t freeAsync(void *ptr, infinirtStream_t stream) {
+    return infinirt::cuda_impl::freeAsync(ptr, stream);
+}
 } // namespace infinirt::cuda
+#endif
+
+#ifdef ENABLE_ILUVATAR_API
+// ILUVATAR namespace - wraps shared implementation
+namespace infinirt::iluvatar {
+infiniStatus_t getDeviceCount(int *count) {
+    return infinirt::cuda_impl::getDeviceCount(count);
+}
+
+infiniStatus_t setDevice(int device_id) {
+    return infinirt::cuda_impl::setDevice(device_id);
+}
+
+infiniStatus_t deviceSynchronize() {
+    return infinirt::cuda_impl::deviceSynchronize();
+}
+
+infiniStatus_t streamCreate(infinirtStream_t *stream_ptr) {
+    return infinirt::cuda_impl::streamCreate(stream_ptr);
+}
+
+infiniStatus_t streamDestroy(infinirtStream_t stream) {
+    return infinirt::cuda_impl::streamDestroy(stream);
+}
+
+infiniStatus_t streamSynchronize(infinirtStream_t stream) {
+    return infinirt::cuda_impl::streamSynchronize(stream);
+}
+
+infiniStatus_t streamWaitEvent(infinirtStream_t stream, infinirtEvent_t event) {
+    return INFINI_STATUS_NOT_IMPLEMENTED;
+}
+
+infiniStatus_t eventCreate(infinirtEvent_t *event_ptr) {
+    return infinirt::cuda_impl::eventCreate(event_ptr);
+}
+
+infiniStatus_t eventCreateWithFlags(infinirtEvent_t *event_ptr, uint32_t flags) {
+    return infinirt::cuda_impl::eventCreateWithFlags(event_ptr, flags);
+}
+
+infiniStatus_t eventRecord(infinirtEvent_t event, infinirtStream_t stream) {
+    return infinirt::cuda_impl::eventRecord(event, stream);
+}
+
+infiniStatus_t eventQuery(infinirtEvent_t event, infinirtEventStatus_t *status_ptr) {
+    return infinirt::cuda_impl::eventQuery(event, status_ptr);
+}
+
+infiniStatus_t eventSynchronize(infinirtEvent_t event) {
+    return infinirt::cuda_impl::eventSynchronize(event);
+}
+
+infiniStatus_t eventDestroy(infinirtEvent_t event) {
+    return infinirt::cuda_impl::eventDestroy(event);
+}
+
+infiniStatus_t eventElapsedTime(float *ms_ptr, infinirtEvent_t start, infinirtEvent_t end) {
+    return infinirt::cuda_impl::eventElapsedTime(ms_ptr, start, end);
+}
+
+infiniStatus_t mallocDevice(void **p_ptr, size_t size) {
+    return infinirt::cuda_impl::mallocDevice(p_ptr, size);
+}
+
+infiniStatus_t mallocHost(void **p_ptr, size_t size) {
+    return infinirt::cuda_impl::mallocHost(p_ptr, size);
+}
+
+infiniStatus_t freeDevice(void *ptr) {
+    return infinirt::cuda_impl::freeDevice(ptr);
+}
+
+infiniStatus_t freeHost(void *ptr) {
+    return infinirt::cuda_impl::freeHost(ptr);
+}
+
+infiniStatus_t memcpy(void *dst, const void *src, size_t size, infinirtMemcpyKind_t kind) {
+    return infinirt::cuda_impl::memcpy(dst, src, size, kind);
+}
+
+infiniStatus_t memcpyAsync(void *dst, const void *src, size_t size, infinirtMemcpyKind_t kind, infinirtStream_t stream) {
+    return infinirt::cuda_impl::memcpyAsync(dst, src, size, kind, stream);
+}
+
+infiniStatus_t mallocAsync(void **p_ptr, size_t size, infinirtStream_t stream) {
+    return infinirt::cuda_impl::mallocAsync(p_ptr, size, stream);
+}
+
+infiniStatus_t freeAsync(void *ptr, infinirtStream_t stream) {
+    return infinirt::cuda_impl::freeAsync(ptr, stream);
+}
+} // namespace infinirt::iluvatar
+#endif
+
+#ifdef ENABLE_QY_API
+// QY namespace - wraps shared implementation
+namespace infinirt::qy {
+infiniStatus_t getDeviceCount(int *count) {
+    return infinirt::cuda_impl::getDeviceCount(count);
+}
+
+infiniStatus_t setDevice(int device_id) {
+    return infinirt::cuda_impl::setDevice(device_id);
+}
+
+infiniStatus_t deviceSynchronize() {
+    return infinirt::cuda_impl::deviceSynchronize();
+}
+
+infiniStatus_t streamCreate(infinirtStream_t *stream_ptr) {
+    return infinirt::cuda_impl::streamCreate(stream_ptr);
+}
+
+infiniStatus_t streamDestroy(infinirtStream_t stream) {
+    return infinirt::cuda_impl::streamDestroy(stream);
+}
+
+infiniStatus_t streamSynchronize(infinirtStream_t stream) {
+    return infinirt::cuda_impl::streamSynchronize(stream);
+}
+
+infiniStatus_t streamWaitEvent(infinirtStream_t stream, infinirtEvent_t event) {
+    return infinirt::cuda_impl::streamWaitEvent(stream, event);
+}
+
+infiniStatus_t eventCreate(infinirtEvent_t *event_ptr) {
+    return infinirt::cuda_impl::eventCreate(event_ptr);
+}
+
+infiniStatus_t eventCreateWithFlags(infinirtEvent_t *event_ptr, uint32_t flags) {
+    return infinirt::cuda_impl::eventCreateWithFlags(event_ptr, flags);
+}
+
+infiniStatus_t eventRecord(infinirtEvent_t event, infinirtStream_t stream) {
+    return infinirt::cuda_impl::eventRecord(event, stream);
+}
+
+infiniStatus_t eventQuery(infinirtEvent_t event, infinirtEventStatus_t *status_ptr) {
+    return infinirt::cuda_impl::eventQuery(event, status_ptr);
+}
+
+infiniStatus_t eventSynchronize(infinirtEvent_t event) {
+    return infinirt::cuda_impl::eventSynchronize(event);
+}
+
+infiniStatus_t eventDestroy(infinirtEvent_t event) {
+    return infinirt::cuda_impl::eventDestroy(event);
+}
+
+infiniStatus_t eventElapsedTime(float *ms_ptr, infinirtEvent_t start, infinirtEvent_t end) {
+    return infinirt::cuda_impl::eventElapsedTime(ms_ptr, start, end);
+}
+
+infiniStatus_t mallocDevice(void **p_ptr, size_t size) {
+    return infinirt::cuda_impl::mallocDevice(p_ptr, size);
+}
+
+infiniStatus_t mallocHost(void **p_ptr, size_t size) {
+    return infinirt::cuda_impl::mallocHost(p_ptr, size);
+}
+
+infiniStatus_t freeDevice(void *ptr) {
+    return infinirt::cuda_impl::freeDevice(ptr);
+}
+
+infiniStatus_t freeHost(void *ptr) {
+    return infinirt::cuda_impl::freeHost(ptr);
+}
+
+infiniStatus_t memcpy(void *dst, const void *src, size_t size, infinirtMemcpyKind_t kind) {
+    return infinirt::cuda_impl::memcpy(dst, src, size, kind);
+}
+
+infiniStatus_t memcpyAsync(void *dst, const void *src, size_t size, infinirtMemcpyKind_t kind, infinirtStream_t stream) {
+    return infinirt::cuda_impl::memcpyAsync(dst, src, size, kind, stream);
+}
+
+infiniStatus_t mallocAsync(void **p_ptr, size_t size, infinirtStream_t stream) {
+    return infinirt::cuda_impl::mallocAsync(p_ptr, size, stream);
+}
+
+infiniStatus_t freeAsync(void *ptr, infinirtStream_t stream) {
+    return infinirt::cuda_impl::freeAsync(ptr, stream);
+}
+} // namespace infinirt::qy
+#endif
+
+#ifdef ENABLE_HYGON_API
+// HYGON namespace - wraps shared implementation
+namespace infinirt::hygon {
+infiniStatus_t getDeviceCount(int *count) {
+    return infinirt::cuda_impl::getDeviceCount(count);
+}
+
+infiniStatus_t setDevice(int device_id) {
+    return infinirt::cuda_impl::setDevice(device_id);
+}
+
+infiniStatus_t deviceSynchronize() {
+    return infinirt::cuda_impl::deviceSynchronize();
+}
+
+infiniStatus_t streamCreate(infinirtStream_t *stream_ptr) {
+    return infinirt::cuda_impl::streamCreate(stream_ptr);
+}
+
+infiniStatus_t streamDestroy(infinirtStream_t stream) {
+    return infinirt::cuda_impl::streamDestroy(stream);
+}
+
+infiniStatus_t streamSynchronize(infinirtStream_t stream) {
+    return infinirt::cuda_impl::streamSynchronize(stream);
+}
+
+infiniStatus_t streamWaitEvent(infinirtStream_t stream, infinirtEvent_t event) {
+    return infinirt::cuda_impl::streamWaitEvent(stream, event);
+}
+
+infiniStatus_t eventCreate(infinirtEvent_t *event_ptr) {
+    return infinirt::cuda_impl::eventCreate(event_ptr);
+}
+
+infiniStatus_t eventCreateWithFlags(infinirtEvent_t *event_ptr, uint32_t flags) {
+    return infinirt::cuda_impl::eventCreateWithFlags(event_ptr, flags);
+}
+
+infiniStatus_t eventRecord(infinirtEvent_t event, infinirtStream_t stream) {
+    return infinirt::cuda_impl::eventRecord(event, stream);
+}
+
+infiniStatus_t eventQuery(infinirtEvent_t event, infinirtEventStatus_t *status_ptr) {
+    return infinirt::cuda_impl::eventQuery(event, status_ptr);
+}
+
+infiniStatus_t eventSynchronize(infinirtEvent_t event) {
+    return infinirt::cuda_impl::eventSynchronize(event);
+}
+
+infiniStatus_t eventDestroy(infinirtEvent_t event) {
+    return infinirt::cuda_impl::eventDestroy(event);
+}
+
+infiniStatus_t eventElapsedTime(float *ms_ptr, infinirtEvent_t start, infinirtEvent_t end) {
+    return infinirt::cuda_impl::eventElapsedTime(ms_ptr, start, end);
+}
+
+infiniStatus_t mallocDevice(void **p_ptr, size_t size) {
+    return infinirt::cuda_impl::mallocDevice(p_ptr, size);
+}
+
+infiniStatus_t mallocHost(void **p_ptr, size_t size) {
+    return infinirt::cuda_impl::mallocHost(p_ptr, size);
+}
+
+infiniStatus_t freeDevice(void *ptr) {
+    return infinirt::cuda_impl::freeDevice(ptr);
+}
+
+infiniStatus_t freeHost(void *ptr) {
+    return infinirt::cuda_impl::freeHost(ptr);
+}
+
+infiniStatus_t memcpy(void *dst, const void *src, size_t size, infinirtMemcpyKind_t kind) {
+    return infinirt::cuda_impl::memcpy(dst, src, size, kind);
+}
+
+infiniStatus_t memcpyAsync(void *dst, const void *src, size_t size, infinirtMemcpyKind_t kind, infinirtStream_t stream) {
+    return infinirt::cuda_impl::memcpyAsync(dst, src, size, kind, stream);
+}
+
+infiniStatus_t mallocAsync(void **p_ptr, size_t size, infinirtStream_t stream) {
+    return infinirt::cuda_impl::mallocAsync(p_ptr, size, stream);
+}
+
+infiniStatus_t freeAsync(void *ptr, infinirtStream_t stream) {
+    return infinirt::cuda_impl::freeAsync(ptr, stream);
+}
+} // namespace infinirt::hygon
+#endif
