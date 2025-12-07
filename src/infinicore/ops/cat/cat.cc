a9f58453a224b8cfa373e843875fb4db845c78ad
JBFYS-XD <jbfysxd@gmail.com>
2025-12-07 17:57:50 +0800

    fix: Optimize the cat operator

--- src/infinicore/ops/cat/cat.cc
+++ src/infinicore/ops/cat/cat.cc
@@ -1,50 +1,110 @@
 #include "infinicore/ops/cat.hpp"
 #include "infinicore/context/context.hpp"
 #include <cstring>
-
+#include <stdexcept>
 namespace infinicore::op {
 
 namespace {
 
-    void low_dim_copy(Tensor& tensor, int dim, Tensor& out, std::byte* tensor_ptr, std::byte* out_ptr, int depth) {
-        if (depth != out->ndim()) {
-            std::byte* now_tensor_ptr = tensor_ptr;
-            std::byte* now_out_ptr = out_ptr;
-            for (int i = 0; i < tensor->shape()[depth]; i ++) {
-                low_dim_copy(tensor, dim, out, now_tensor_ptr, now_out_ptr, depth + 1);
-                now_tensor_ptr += tensor->stride(depth) * dsize(tensor->dtype());
-                now_out_ptr += out->stride(depth) * dsize(out->dtype());
-            }
-        } else {
-            Size data_size = dsize(out->dtype());
-            if (out->device().getType() == Device::Type::CPU)
-                std::memcpy(out_ptr, tensor_ptr, data_size);
-            else
-                context::memcpyD2D(out_ptr, tensor_ptr, data_size);
-        }
-    }
+class CatInfo {
+    
+    CatInfo() = default;
 
-    void high_dim_split(std::vector<Tensor>& tensors, int dim, Tensor& out, std::vector<std::byte*> tensors_ptr, std::byte* out_ptr, int depth) {
-        if (depth != dim) {
-            std::vector<std::byte*> now_tensors_ptr = tensors_ptr;
-            std::byte* now_out_ptr = out_ptr;
-            for (int i = 0; i < out->shape()[depth]; i ++) {
-                high_dim_split(tensors, dim, out, now_tensors_ptr, now_out_ptr, depth + 1);
-                for (int i = 0; i < tensors.size(); i ++) {
-                    if (tensors[i]->ndim() == 1)    continue;
-                    now_tensors_ptr[i] += tensors[i]->stride(depth) * dsize(tensors[i]->dtype());
+public:
+
+    int dim;
+    int ndim;
+    size_t tensors_size;
+    
+    std::vector<int> contiguous_dim;
+    std::vector<size_t> copy_size;
+
+    static CatInfo create(Tensor& out, std::vector<Tensor>& tensors, int dim) {
+        
+        int ndim = out->ndim();
+        size_t tensors_size = tensors.size();
+
+        std::vector<int> contiguous_dim(tensors_size, ndim);
+        std::vector<size_t> copy_size(tensors_size, dsize(out->dtype()));
+
+        for (int i = 0; i < tensors_size; i ++) {
+            if (tensors[i]->ndim() == 1) continue;
+            if (tensors[i]->stride(ndim - 1) == 1) {
+                contiguous_dim[i] = ndim - 1;
+                for (int j = ndim - 2; j >= dim; j --) {
+                    if (tensors[i]->stride(j) == tensors[i]->stride(j + 1) * tensors[i]->shape()[j + 1]) {
+                        contiguous_dim[i] = j;
+                    }
                 }
-                now_out_ptr += out->stride(depth) * dsize(out->dtype());
             }
-        } else {
-            std::byte* now_out_ptr = out_ptr;
-            for (int i = 0; i < tensors.size(); i ++) {
-                if (tensors[i]->ndim() == 1)    continue;
-                low_dim_copy(tensors[i], dim, out, tensors_ptr[i], now_out_ptr, depth);
-                now_out_ptr += tensors[i]->shape()[depth] * out->stride(depth) * dsize(out->dtype());
+            for (int j = contiguous_dim[i]; j < ndim; j ++) {
+                copy_size[i] *= tensors[i]->shape()[j];
             }
         }
+
+        return CatInfo{dim, ndim, tensors_size, contiguous_dim, copy_size};
     }
+};
+
+
+void low_dim_copy(
+    CatInfo &info, Tensor& tensor, Tensor& out, 
+    std::byte *tensor_ptr, std::byte *out_ptr, 
+    int depth, int tensor_pos) {
+
+    if (depth != info.contiguous_dim[tensor_pos]) {
+        std::byte* now_tensor_ptr = tensor_ptr;
+        std::byte* now_out_ptr = out_ptr;
+
+        for (int i = 0; i < tensor->shape()[depth]; i ++) {
+        
+            low_dim_copy(info, tensor, out, now_tensor_ptr, now_out_ptr, depth + 1, tensor_pos);
+        
+            now_tensor_ptr += tensor->stride(depth) * dsize(tensor->dtype());
+            now_out_ptr += out->stride(depth) * dsize(out->dtype());
+        }
+    } else {
+        if (out->device().getType() == Device::Type::CPU) {
+
+            std::memcpy(out_ptr, tensor_ptr, info.copy_size[tensor_pos]);
+        } else {
+            
+            context::memcpyD2D(out_ptr, tensor_ptr, info.copy_size[tensor_pos]);
+        }
+    }
+}
+
+void high_dim_split(
+    CatInfo &info, std::vector<Tensor>& tensors, Tensor& out, 
+    std::vector<std::byte*> tensors_ptr, std::byte* out_ptr, 
+    int depth) {
+    
+    if (depth != info.dim) {
+        std::vector<std::byte*> now_tensors_ptr = tensors_ptr;
+        std::byte* now_out_ptr = out_ptr;
+
+        for (int i = 0; i < out->shape()[depth]; i ++) {
+
+            high_dim_split(info, tensors, out, now_tensors_ptr, now_out_ptr, depth + 1);
+            
+            for (int i = 0; i < info.tensors_size; i ++) {
+                if (tensors[i]->ndim() == 1)    continue;
+                now_tensors_ptr[i] += tensors[i]->stride(depth) * dsize(tensors[i]->dtype());
+            }
+            now_out_ptr += out->stride(depth) * dsize(out->dtype());
+        }
+    } else {
+        std::byte* now_out_ptr = out_ptr;
+
+        for (int i = 0; i < info.tensors_size; i ++) {
+            if (tensors[i]->ndim() == 1)    continue;
+            
+            low_dim_copy(info, tensors[i], out, tensors_ptr[i], now_out_ptr, depth, i);
+            
+            now_out_ptr += tensors[i]->shape()[depth] * out->stride(depth) * dsize(out->dtype());
+        }
+    }
+}
 
 } // namespace
 
@@ -67,13 +127,13 @@ Tensor cat(std::vector<Tensor> tensors, int dim) {
 }
 
 void cat_(Tensor out, std::vector<Tensor> tensors, int dim) {
+    // assert the parameter properties are correct.
     assert(tensors.size() >= 2);
     int ndim = out->ndim();
     assert(-ndim <= dim && dim < ndim);
     dim = (dim + ndim) % ndim;
 
-
-    Size dim_shape = 0;
+    size_t dim_shape = 0;
     for (auto& tensor : tensors) {
         assert(tensor->ndim() == ndim || tensors[i]->ndim() == 1);
         if (tensor->ndim() == 1) {
@@ -90,14 +150,15 @@ void cat_(Tensor out, std::vector<Tensor> tensors, int dim) {
     }
     assert(dim_shape == out->shape()[dim]);
 
+    // Get info
+    CatInfo info = CatInfo::create(out, tensors, dim);
     std::vector<std::byte*> tensors_ptr(tensors.size());
-
     for (int i = 0; i < tensors.size(); i ++) {
         tensors_ptr[i] = tensors[i]->data();
     }
     std::byte* out_ptr = out->data();
 
-    high_dim_split(tensors, dim, out, tensors_ptr, out_ptr, 0);
+    high_dim_split(info, tensors, out, tensors_ptr, out_ptr, 0);
 
 }
 
