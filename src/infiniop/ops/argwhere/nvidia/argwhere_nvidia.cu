7568bbcb1ac2925b42ffe093ef5ea2bd3b4f1a32
yudeng <2272776615@qq.com>
2025-12-14 17:24:57 +0800

    iluvatar version finished

--- src/infiniop/ops/argwhere/nvidia/argwhere_nvidia.cu
+++ src/infiniop/ops/argwhere/nvidia/argwhere_nvidia.cu
@@ -5,25 +5,24 @@
 #include "infinicore.h"
 #include <cstddef>
 #include <cstdint>
-#include <spdlog/spdlog.h>
 
 infiniStatus_t launchKernel(const void *data, int64_t *results, size_t N,
                             size_t M, const size_t *shapes,
                             const ptrdiff_t *strides, size_t ndim,
                             infiniDtype_t dtype, size_t *count) {
 
-  if (dtype == INFINI_DTYPE_F32) {
-    parallel_block_argwhere_kernel<float><<<1, M / 2, M>>>(
-        (float *)data, results, N, shapes, strides, ndim, count);
-  } else {
-    return INFINI_STATUS_BAD_TENSOR_DTYPE;
-  }
-  return INFINI_STATUS_SUCCESS;
+    if (dtype == INFINI_DTYPE_F32) {
+        parallel_block_argwhere_kernel<float><<<1, M / 2, M>>>(
+            (float *)data, results, N, shapes, strides, ndim, count);
+    } else {
+        return INFINI_STATUS_BAD_TENSOR_DTYPE;
+    }
+    return INFINI_STATUS_SUCCESS;
 }
 
 namespace op::argwhere::nvidia {
 struct Descriptor::Opaque {
-  std::shared_ptr<device::nvidia::Handle::Internal> internal;
+    std::shared_ptr<device::nvidia::Handle::Internal> internal;
 };
 
 Descriptor::~Descriptor() { delete _opaque; }
@@ -31,69 +30,66 @@ Descriptor::~Descriptor() { delete _opaque; }
 infiniStatus_t Descriptor::create(infiniopHandle_t handle,
                                   Descriptor **desc_ptr,
                                   infiniopTensorDescriptor_t x_desc) {
-  auto info = ArgwhereInfo::create(x_desc);
-  CHECK_RESULT(info);
-  size_t workspace_size = x_desc->ndim() * sizeof(size_t) * 2 +
-                          x_desc->ndim() * sizeof(int64_t) * x_desc->numel() +
-                          sizeof(size_t);
-  *desc_ptr = new Descriptor(
-      info.take(), workspace_size,
-      new Opaque{
-          reinterpret_cast<device::nvidia::Handle *>(handle)->internal()},
-      handle->device, handle->device_id);
-  return INFINI_STATUS_SUCCESS;
+    auto info = ArgwhereInfo::create(x_desc);
+    CHECK_RESULT(info);
+    size_t workspace_size = x_desc->ndim() * sizeof(size_t) * 2 + x_desc->ndim() * sizeof(int64_t) * x_desc->numel() + sizeof(size_t);
+    *desc_ptr = new Descriptor(
+        info.take(), workspace_size,
+        new Opaque{
+            reinterpret_cast<device::nvidia::Handle *>(handle)->internal()},
+        handle->device, handle->device_id);
+    return INFINI_STATUS_SUCCESS;
 }
 
 int nextPowerOfTwo(int x) {
-  int power = 1;
-  while (power < x) {
-    power *= 2;
-  }
-  return power;
+    int power = 1;
+    while (power < x) {
+        power *= 2;
+    }
+    return power;
 }
 
 infiniStatus_t Descriptor::calculate(void *workspace, size_t workspace_size,
                                      void **y, size_t *count, const void *x,
                                      void *stream) const {
-  cudaStream_t cuda_stream = static_cast<cudaStream_t>(stream);
-  size_t ndim = _info.strides.size();
-  ptrdiff_t *strides = static_cast<ptrdiff_t *>(workspace);
-  size_t *shapes = reinterpret_cast<size_t *>(strides + ndim);
-  int64_t *result = reinterpret_cast<int64_t *>(shapes + ndim);
-  size_t *count_cuda =
-      reinterpret_cast<size_t *>(result + _info.num_elements * ndim);
+    cudaStream_t cuda_stream = static_cast<cudaStream_t>(stream);
+    size_t ndim = _info.strides.size();
+    ptrdiff_t *strides = static_cast<ptrdiff_t *>(workspace);
+    size_t *shapes = reinterpret_cast<size_t *>(strides + ndim);
+    int64_t *result = reinterpret_cast<int64_t *>(shapes + ndim);
+    size_t *count_cuda = reinterpret_cast<size_t *>(result + _info.num_elements * ndim);
 
-  cudaMemcpyAsync(shapes, _info.shapes.data(),
-                  _info.shapes.size() * sizeof(size_t), cudaMemcpyHostToDevice,
-                  cuda_stream);
-  cudaMemcpyAsync(strides, _info.strides.data(),
-                  _info.strides.size() * sizeof(ptrdiff_t),
-                  cudaMemcpyHostToDevice, cuda_stream);
-  // cudaStreamSynchronize(cuda_stream);
-  size_t M = nextPowerOfTwo(_info.num_elements);
-  CHECK_STATUS(launchKernel(x, result, _info.num_elements, M, shapes, strides,
-                            ndim, INFINI_DTYPE_F32, count_cuda));
-  // 从设备内存中读取 count_cuda 的值
-  cudaMemcpyAsync(count, count_cuda, sizeof(size_t), cudaMemcpyDeviceToHost);
-  cudaStreamSynchronize(cuda_stream);
+    cudaMemcpyAsync(shapes, _info.shapes.data(),
+                    _info.shapes.size() * sizeof(size_t), cudaMemcpyHostToDevice,
+                    cuda_stream);
+    cudaMemcpyAsync(strides, _info.strides.data(),
+                    _info.strides.size() * sizeof(ptrdiff_t),
+                    cudaMemcpyHostToDevice, cuda_stream);
+    // cudaStreamSynchronize(cuda_stream);
+    size_t M = nextPowerOfTwo(_info.num_elements);
+    CHECK_STATUS(launchKernel(x, result, _info.num_elements, M, shapes, strides,
+                              ndim, INFINI_DTYPE_F32, count_cuda));
+    // 从设备内存中读取 count_cuda 的值
+    cudaMemcpyAsync(count, count_cuda, sizeof(size_t), cudaMemcpyDeviceToHost);
+    cudaStreamSynchronize(cuda_stream);
 
-  //写回结果
-  *y = new int64_t[(*count) * ndim];
-  // cudaStreamSynchronize(cuda_stream);
+    // 写回结果
+    *y = new int64_t[(*count) * ndim];
+    // cudaStreamSynchronize(cuda_stream);
 
-  // spdlog::debug("count_cuda:{}", *count_cuda);
-  //   spdlog::debug("N:{}", N);
+    // spdlog::debug("count_cuda:{}", *count_cuda);
+    //   spdlog::debug("N:{}", N);
 
-  cudaMemcpyAsync(*y, result, sizeof(int64_t) * (*count) * ndim,
-                  cudaMemcpyDeviceToHost, cuda_stream);
+    cudaMemcpyAsync(*y, result, sizeof(int64_t) * (*count) * ndim,
+                    cudaMemcpyDeviceToHost, cuda_stream);
 
-  // cudaStreamSynchronize(cuda_stream);
-  // for (size_t i = 0; i < (*count) * ndim; i++) {
-  //   spdlog::debug("(*y)[{}]:{}", i, static_cast<size_t *>(*y)[i]);
-  // }
-  // cudaFreeAsync(result, cuda_stream);
-  // cudaFreeAsync(count_cuda, cuda_stream);
-  return INFINI_STATUS_SUCCESS;
+    // cudaStreamSynchronize(cuda_stream);
+    // for (size_t i = 0; i < (*count) * ndim; i++) {
+    //   spdlog::debug("(*y)[{}]:{}", i, static_cast<size_t *>(*y)[i]);
+    // }
+    // cudaFreeAsync(result, cuda_stream);
+    // cudaFreeAsync(count_cuda, cuda_stream);
+    return INFINI_STATUS_SUCCESS;
 }
 
 } // namespace op::argwhere::nvidia
--- src/infiniop/ops/fmin/cuda/kernel.cuh
+++ src/infiniop/ops/fmin/cuda/kernel.cuh
@@ -1,23 +1,32 @@
 #ifndef __FMIN_CUDA_H__
 #define __FMIN_CUDA_H__
 
+#include <type_traits>
 namespace op::fmin::cuda {
 typedef struct FminOp {
 public:
-  static constexpr size_t num_inputs = 2;
-  template <typename T>
-  __device__ __forceinline__ T operator()(const T &a, const T &b) const {
-    if constexpr (std::is_same_v<T, half2>) {
-      return __hmin2(a, b);
-    } else if constexpr (std::is_same_v<T, half> ||
-                         std::is_same_v<T, cuda_bfloat16>) {
-      return __hmin(a, b);
-    } else if constexpr (std::is_same_v<T, float>) {
-      return fminf(a, b);
-    } else {
-      return a < b ? a : b;
+    static constexpr size_t num_inputs = 2;
+    template <typename T>
+    __device__ __forceinline__ T operator()(const T &a, const T &b) const {
+#if defined(ENABLE_ILUVATAR_API)
+        if constexpr (std::is_same_v<T, cuda_bfloat16>) {
+            float a_f = __bfloat162float(a), b_f = __bfloat162float(b);
+            return __float2bfloat16(fminf(a_f, b_f));
+        } else if constexpr (std::is_same_v<T, half>) {
+            float a_f = __half2float(a), b_f = __half2float(b);
+            return __float2half(fminf(a_f, b_f));
+        }
+#elif defined(ENABLE_NVIDIA_API)
+        if constexpr (std::is_same_v<T, half> || std::is_same_v < T, cuda_bfloat16) {
+            return __hmin(a, b);
+        }
+#endif
+        else if constexpr (std::is_same_v<T, float>) {
+            return fminf(a, b);
+        } else {
+            return a < b ? a : b;
+        }
     }
-  }
 } FminOp;
 } // namespace op::fmin::cuda
 
