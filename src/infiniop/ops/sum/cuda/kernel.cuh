f5a541c1f65796b04ebe989f4537e016add0bc20
root <root@DY-20230804LNIS>
2025-12-19 02:17:48 +0800

    fix moore bug in kernel.cuh

--- src/infiniop/ops/sum/cuda/kernel.cuh
+++ src/infiniop/ops/sum/cuda/kernel.cuh
@@ -37,7 +37,8 @@ __global__ void sumAllKernel(
         size_t input_offset = indexToOffset(idx, permuted_input_shape_size, permuted_input_shape, permuted_input_strides);
         s_data[tid] = input[input_offset];
     } else {
-        s_data[tid] = T(0);
+        // s_data[tid] = T(0);
+        s_data[tid] = static_cast<T>(0.f);
     }
     __syncthreads();
     for(size_t s = blockDim.x / 2; s > 0; s >>=1){
@@ -71,7 +72,8 @@ __global__ void sumKernel(
     size_t idx = tid + blockIdx.x * blockDim.x;
     if(idx >= output_size) return;
     size_t output_index = indexToOffset(idx, output_shape_size, output_shape, output_strides);
-    T tempSum = T(0);
+    // T tempSum = T(0);
+    T tempSum = static_cast<T>(0.f);
     for(size_t i = 0; i < reduce_num; i++){
         size_t input_offset = indexToOffset(i + idx * reduce_num, permuted_input_shape_size, permuted_input_shape, permuted_input_strides);
         tempSum += input[input_offset];
