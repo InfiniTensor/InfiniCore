9fbc0b3d0555d9f38bbecefd629c72ff73e37b0d
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-01-13 12:39:00 +0000

    issue/919 - Add a NineToothed implementation of `scaled_dot_product_attention`

--- python/infinicore/nn/functional/__init__.py
+++ python/infinicore/nn/functional/__init__.py
@@ -4,6 +4,7 @@ from .linear import linear
 from .random_sample import random_sample
 from .rms_norm import rms_norm
 from .rope import RopeAlgo, rope
+from .scaled_dot_product_attention import scaled_dot_product_attention
 from .silu import silu
 from .swiglu import swiglu
 
@@ -11,6 +12,7 @@ __all__ = [
     "causal_softmax",
     "random_sample",
     "rms_norm",
+    "scaled_dot_product_attention",
     "silu",
     "swiglu",
     "linear",
--- src/infinicore/pybind11/ops.hpp
+++ src/infinicore/pybind11/ops.hpp
@@ -7,6 +7,7 @@
 #include "ops/attention.hpp"
 #include "ops/causal_softmax.hpp"
 #include "ops/embedding.hpp"
+#include "ops/flash_attention.hpp"
 #include "ops/linear.hpp"
 #include "ops/matmul.hpp"
 #include "ops/mul.hpp"
@@ -29,6 +30,7 @@ inline void bind(py::module &m) {
     bind_add_rms_norm(m);
     bind_attention(m);
     bind_causal_softmax(m);
+    bind_flash_attention(m);
     bind_random_sample(m);
     bind_linear(m);
     bind_matmul(m);
