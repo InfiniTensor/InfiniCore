581d4a4bfa8a00c16d88160a6e8b90849de3c458
qinyiqun <qinyiqun@outlook.com>
2026-01-15 10:35:28 +0800

    w8: support multiple batch size

--- src/infinicore/ops/linear_w8a8i8/linear_w8a8i8.cc
+++ src/infinicore/ops/linear_w8a8i8/linear_w8a8i8.cc
@@ -8,24 +8,17 @@ Tensor linear_w8a8i8(Tensor input,
                      Tensor weight_packed,
                      Tensor weight_scale,
                      std::optional<Tensor> bias) {
+
     Size ndim = input->ndim();
     Size out_features = weight_packed->shape()[0];
+
+    // Assign memory to out variables
     auto output_shape = input->shape();
     output_shape[ndim - 1] = out_features;
-    // tempe for batch=1
     auto out = Tensor::empty(output_shape, input->dtype(), input->device());
-    linear_w8a8i8_(out->view({output_shape[1], output_shape[2]}),
-                   input->view({input->shape()[1], input->shape()[2]}),
-                   weight_packed,
-                   weight_scale,
-                   bias);
-    // auto out = Tensor::empty(output_shape, input->dtype(), input->device());
-    // linear_w8a8i8_(out,
-    //                input,
-    //                weight_packed,
-    //                weight_scale,
-    //                bias);
-    // out->debug();
+
+    // Inplace Calculate
+    linear_w8a8i8_(out, input, weight_packed, weight_scale, bias);
     return out;
 }
 
@@ -35,18 +28,33 @@ void linear_w8a8i8_(Tensor out,
                     Tensor weight_scale,
                     std::optional<Tensor> bias) {
 
+    auto weight_packed_shape = weight_packed->shape();
+    Size out_features = weight_packed_shape[0];
+    Size in_features = weight_packed_shape[1];
+
+    Size ndim = input->ndim();
+    assert(out->ndim() == ndim);
+
+    Size N = 1;
+    auto input_shape = input->shape();
+    for (size_t i = 0; i < ndim - 1; ++i) {
+        N *= input_shape[i];
+    }
+
     auto input_packed = Tensor::empty(
-        input->shape(),
+        {N, input_shape[ndim - 1]},
         DataType::I8,
         input->device());
     auto input_scale = Tensor::empty(
-        {input->shape()[0], 1},
+        {N, 1},
         DataType::F32,
         input->device());
-    op::per_channel_quant_i8_(input, input_packed, input_scale);
-
+    op::per_channel_quant_i8_(input->view({N, in_features}), input_packed, input_scale);
+    if (bias.has_value()) {
+        bias = std::make_optional(bias.value()->as_strided({N, out_features}, {0, 1}));
+    }
     op::scaled_mm_i8_(
-        out,
+        out->view({N, out_features}),
         input_packed,
         input_scale,
         weight_packed->permute({1, 0}),
