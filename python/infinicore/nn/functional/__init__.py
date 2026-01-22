036fb810d3257787120f46f6303ee97eb0306949
qinyiqun <qinyiqun@outlook.com>
2026-01-22 14:12:55 +0800

    fix format

--- python/infinicore/nn/functional/__init__.py
+++ python/infinicore/nn/functional/__init__.py
@@ -2,12 +2,12 @@ from .causal_softmax import causal_softmax
 from .embedding import embedding
 from .flash_attention import flash_attention
 from .linear import linear
+from .linear_w8a8i8 import linear_w8a8i8
 from .random_sample import random_sample
 from .rms_norm import rms_norm
 from .rope import RopeAlgo, rope
 from .silu import silu
 from .swiglu import swiglu
-from .linear_w8a8i8 import linear_w8a8i8
 
 __all__ = [
     "causal_softmax",
