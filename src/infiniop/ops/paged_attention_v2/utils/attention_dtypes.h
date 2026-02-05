3ef80074703082827999cf108596e66f89590b61
pengcheng888 <wangpengcheng@example.com>
2026-02-05 12:17:29 +0000

    issue/1004 - metax build

--- src/infiniop/ops/paged_attention_v2/utils/attention_dtypes.h
+++ src/infiniop/ops/paged_attention_v2/utils/attention_dtypes.h
@@ -6,6 +6,11 @@
 #include "dtype_bfloat16.cuh"
 #include "dtype_float32.cuh"
 #include "dtype_fp8.cuh"
+
 #if defined(ENABLE_NVIDIA_API)
 #include "../nvidia/dtype_float16.cuh"
+#endif
+
+#if defined(ENABLE_METAX_API)
+#include "../metax/dtype_float16.cuh"
 #endif
