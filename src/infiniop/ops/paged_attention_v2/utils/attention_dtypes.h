f205057e1eecd8927d4fdf2d8ebb7375e3766d63
pengcheng888 <wangpengcheng@example.com>
2026-02-06 14:42:05 +0800

    issue/1004- metax failed

--- src/infiniop/ops/paged_attention_v2/utils/attention_dtypes.h
+++ src/infiniop/ops/paged_attention_v2/utils/attention_dtypes.h
@@ -8,9 +8,12 @@
 #include "dtype_fp8.cuh"
 
 #if defined(ENABLE_NVIDIA_API)
+#include "../nvidia/dtype_bfloat16.cuh"
 #include "../nvidia/dtype_float16.cuh"
+
 #endif
 
 #if defined(ENABLE_METAX_API)
+#include "../metax/dtype_bfloat16.cuh"
 #include "../metax/dtype_float16.cuh"
 #endif
