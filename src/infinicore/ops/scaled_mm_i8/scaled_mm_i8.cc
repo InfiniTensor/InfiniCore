75984cf3a8d8146f1716a6a66fde6a10fbb21610
wooway777 <wooway777@gmail.com>
2026-02-11 02:42:01 +0000

    demo131 - remove commented lines

--- src/infinicore/ops/scaled_mm_i8/scaled_mm_i8.cc
+++ src/infinicore/ops/scaled_mm_i8/scaled_mm_i8.cc
@@ -4,16 +4,6 @@
 
 namespace infinicore::op {
 
-// common::OpDispatcher<ScaledMMI8::schema> &ScaledMMI8::dispatcher() {
-//     static common::OpDispatcher<ScaledMMI8::schema> dispatcher_;
-//     return dispatcher_;
-// };
-
-// void ScaledMMI8::execute(Tensor c, Tensor a_p, Tensor a_s, Tensor b_p, Tensor b_s, std::optional<Tensor> bias) {
-//     INFINICORE_ASSERT_TENSORS_SAME_DEVICE(c, a_p, a_s, b_p, b_s);
-//     infinicore::context::setDevice(c->device());
-//     dispatcher().lookup(c->device().getType())(c, a_p, a_s, b_p, b_s, bias);
-// }
 INFINICORE_GRAPH_OP_DISPATCHERS_IMPL(I8Gemm);
 
 I8Gemm::I8Gemm(Tensor c, const Tensor &a_p, const Tensor &a_s, const Tensor &b_p, const Tensor &b_s, std::optional<Tensor> bias) {
