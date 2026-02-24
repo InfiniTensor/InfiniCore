ce3f99b66bbe353ccfd821b763415503037fb061
pengcheng888 <1033693766@qq.com>
2026-02-24 10:26:17 +0800

    format

--- python/infinicore/nn/functional/__init__.py
+++ python/infinicore/nn/functional/__init__.py
@@ -4,23 +4,23 @@ from .flash_attention import flash_attention
 from .linear import linear
 from .paged_attention_v2 import paged_attention_v2
 from .random_sample import random_sample
+from .reshape_and_cache import reshape_and_cache
 from .rms_norm import rms_norm
 from .rope import RopeAlgo, rope
 from .silu import silu
 from .swiglu import swiglu
-from .reshape_and_cache import reshape_and_cache
 
 __all__ = [
     "causal_softmax",
     "embedding",
     "flash_attention",
     "linear",
+    "paged_attention_v2",
     "random_sample",
+    "reshape_and_cache",
     "rms_norm",
     "RopeAlgo",
     "rope",
     "silu",
     "swiglu",
-    "paged_attention_v2",
-    "reshape_and_cache",
 ]
