351dba464de77aa6d97f2a6892bca5c36e03c007
GordonYang1 <1468121796@qq.com>
2026-03-25 16:53:40 +0800

    issue/1100 format cosh C++ files with clang-format

--- src/infinicore/ops/cosh/cosh_infiniop.cc
+++ src/infinicore/ops/cosh/cosh_infiniop.cc
@@ -1,7 +1,7 @@
 #include "../../utils.hpp"
 #include "infinicore/common/hash.hpp"
-#include "infinicore/ops/cosh.hpp"
 #include "infinicore/ops/common/cache.hpp"
+#include "infinicore/ops/cosh.hpp"
 #include <infiniop.h>
 
 namespace infinicore::op::cosh_impl::infiniop {
