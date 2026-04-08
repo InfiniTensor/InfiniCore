a9819f8a8627759e0a2d65c4615266c54f9ba420
gongchensu <zhuyue_134@qq.com>
2026-04-08 09:11:05 +0000

    fix(unary-cuda): improve acos/reciprocal/tan numeric correctness

--- src/infiniop/ops/acos/cuda/kernel.cuh
+++ src/infiniop/ops/acos/cuda/kernel.cuh
@@ -8,17 +8,6 @@
 namespace op::acos::cuda {
 
 // ----------------------
-// Fast acos approximation
-// ----------------------
-__device__ __forceinline__ float fast_acosf(float x) {
-    // 高性能多项式近似 acos(x)
-    float ax = fabsf(x);
-    float t = sqrtf(1.0f - ax);
-    float r = ((-0.0187293f * ax + 0.0742610f) * ax - 0.2121144f) * ax + 1.5707288f;
-    return (x >= 0.0f ? t * r : 3.14159265358979323846f - t * r);
-}
-
-// ----------------------
 // float kernel (F32)
 // ----------------------
 template <typename T>
@@ -26,7 +15,7 @@ __device__ __forceinline__ T acos_impl(T val);
 
 template <>
 __device__ __forceinline__ float acos_impl<float>(float val) {
-    return fast_acosf(val);
+    return ::acosf(val);
 }
 
 // ----------------------
@@ -34,13 +23,8 @@ __device__ __forceinline__ float acos_impl<float>(float val) {
 // ----------------------
 template <>
 __device__ __forceinline__ half acos_impl<half>(half val) {
-#if (__CUDA_ARCH__ >= 530)
     float f = __half2float(val);
-    return __float2half(fast_acosf(f));
-#else
-    float f = __half2float(val);
-    return __float2half(fast_acosf(f));
-#endif
+    return __float2half(::acosf(f));
 }
 
 // ----------------------
@@ -48,17 +32,10 @@ __device__ __forceinline__ half acos_impl<half>(half val) {
 // ----------------------
 template <>
 __device__ __forceinline__ half2 acos_impl<half2>(half2 val) {
-#if (__CUDA_ARCH__ >= 530)
     float2 f = __half22float2(val);
-    f.x = fast_acosf(f.x);
-    f.y = fast_acosf(f.y);
+    f.x = ::acosf(f.x);
+    f.y = ::acosf(f.y);
     return __float22half2_rn(f);
-#else
-    float2 f = __half22float2(val);
-    f.x = fast_acosf(f.x);
-    f.y = fast_acosf(f.y);
-    return __float22half2_rn(f);
-#endif
 }
 
 // ----------------------
@@ -67,7 +44,12 @@ __device__ __forceinline__ half2 acos_impl<half2>(half2 val) {
 template <>
 __device__ __forceinline__ cuda_bfloat16 acos_impl<cuda_bfloat16>(cuda_bfloat16 val) {
     float f = __bfloat162float(val);
-    return __float2bfloat16(fast_acosf(f));
+    return __float2bfloat16(::acosf(f));
+}
+
+template <>
+__device__ __forceinline__ double acos_impl<double>(double val) {
+    return ::acos(val);
 }
 
 // ----------------------
@@ -75,7 +57,7 @@ __device__ __forceinline__ cuda_bfloat16 acos_impl<cuda_bfloat16>(cuda_bfloat16 
 // ----------------------
 template <typename T>
 __device__ __forceinline__ T acos_impl(T val) {
-    return static_cast<T>(fast_acosf(static_cast<float>(val)));
+    return static_cast<T>(::acos(static_cast<double>(val)));
 }
 
 // ----------------------
